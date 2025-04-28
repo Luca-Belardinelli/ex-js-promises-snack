aeeb255ca3d61b226394a6b84f95582e67904af4
Luca Belardinelli <belardinelliluca@gmail.com>
2025-04-28 18:11:29 +0200

    Fix HTML closing tag and clean up JavaScript code structure

--- snack1.js
+++ snack1.js
@@ -3,6 +3,26 @@
 
 // Crea una funzione getPostTitle(id) che accetta un id
 // e restituisce una Promise che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+
+
+
+const getPostTitle = id => {
+    const promessa = new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(data => resolve(data.title))
+            .catch(reject)
+    });
+    return promessa;
+}
+
+getPostTitle(12)
+    .then(titolo => console.log("Titolo:", titolo))
+    .catch(error => console.error(error));
+
+
+
+
 // 🎯 Bonus: Ottieni l'intero post con l'autore
 // Crea una funzione getPost(id) che recupera l'intero post.
 // Concatena una seconda chiamata che aggiunge una proprietà user che contiene i dati dell'autore,
