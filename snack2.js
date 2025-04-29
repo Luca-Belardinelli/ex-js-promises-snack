492324ba2fdfbf069acc73d7c6e109fecc5b11d6
Luca Belardinelli <belardinelliluca@gmail.com>
2025-04-29 09:50:09 +0200

    Implement creaLanciaDado function to create a closure that remembers the last dice roll

--- snack2.js
+++ snack2.js
@@ -32,3 +32,35 @@ lanciaDado()
 // 🎯 Bonus: HOF con closure per memorizzare l'ultimo lancio
 // Modifica la funzione in creaLanciaDado(), che restituisce una closure che memorizza l'ultimo risultato.
 // Se il numero esce due volte di fila, stampa "Incredibile!".s
+
+
+function creaLanciaDado() {
+
+    let ultimoLancio = null;
+
+
+    return function () {
+        return new Promise((resolve, reject) => {
+            console.log("Sto lanciando il dado...");
+            setTimeout(() => {
+                if (Math.floor(Math.random() * 10) + 1) {
+                    ultimoLancio = null;
+                    reject("Il dado è tratto 2")
+                } else {
+                    const dado = Math.floor(Math.random() * 6) + 1;
+                    if (dado === ultimoLancio) {
+                        console.log("let's go")
+                    }
+                    ultimoLancio = dado;
+                    resolve(dado)
+                }
+            }, 3000)
+        })
+    }
+}
+
+const lanciaDadoConMemoria = creaLanciaDado();
+
+lanciaDadoConMemoria()
+    .then(dado => console.log("Questo è il numero:", dado))
+    .catch(error => console.log(error))
